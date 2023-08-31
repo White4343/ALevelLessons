1b9166301c85fd6b79cbde9fecb762aaffd4f864
Андрей Тарасенко <mikatohimemiya@gmail.com>
2023-08-31 12:19:24 +0300

    Fixed potential memory leak.

--- LoggerApp/Logger.cs
+++ LoggerApp/Logger.cs
@@ -8,44 +8,26 @@ namespace LoggerApp
 {
     public sealed class Logger
     {
-        private readonly DateTime _dateOfLog = DateTime.Now;
-        private string _logType;
-        private string _message;
-
         private static readonly Logger instance = new Logger();
         private List<object> _logs = new List<object>();
 
-        public static Logger Instance
-        {
-            get => instance;
-        }
+        public static Logger Instance => instance;
 
         static Logger() {}
 
         private Logger() {}
 
-        private Logger(DateTime dateOfLog, string logType, string message)
-        {
-            _dateOfLog = dateOfLog;
-            _logType = logType;
-            _message = message;
-        }
-
         public void AddLogData(string logType, string message)
         {
-            _logs.Add(new Logger(_dateOfLog, logType, message));
+            _logs.Add($"{DateTime.Now}: {logType}: {message}");
         }
 
-        public override string ToString()
-        {
-            return $"{_dateOfLog}: {_logType}: {_message}";
-        }
 
         public void PrintLoggedData()
         {
             foreach (var log in _logs)
             {
-                Console.WriteLine(log.ToString());
+                Console.WriteLine(log);
             }
         }
     }
