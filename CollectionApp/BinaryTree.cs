5299b85b2001eb78b7df06fccdb3b25fd89c5712
Андрей Тарасенко <mikatohimemiya@gmail.com>
2023-09-25 17:05:33 +0300

    Added solution for Collection's HW.

---
+++ CollectionApp/BinaryTree.cs
@@ -0,0 +1,170 @@
+﻿namespace CollectionApp
+{
+    public class Node<T> where T : IComparable<T>
+    {
+        public T Value { get; private set; }
+        public Node<T> Left { get; private set; } = null!;
+        public Node<T> Right { get; private set; } = null!;
+
+        public Node(T value) => Value = value;
+
+        public void Add(T newValue)
+        {
+            if (newValue.CompareTo(Value) < 0)
+            {
+                if (Left == null)
+                {
+                    Left = new Node<T>(newValue);
+                }
+                else
+                {
+                    Left.Add(newValue);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = new Node<T>(newValue);
+                }
+                else
+                {
+                    Right.Add(newValue);
+                }
+            }
+        }
+
+        public IEnumerable<T> InOrderTraversal()
+        {
+            if (Left != null)
+            {
+                foreach (T value in Left.InOrderTraversal())
+                {
+                    yield return value;
+                }
+            }
+
+            yield return Value;
+
+            if (Right != null)
+            {
+                foreach (T value in Right.InOrderTraversal())
+                {
+                    yield return value;
+                }
+            }
+        }
+
+        public IEnumerable<T> PreOrderTraversal()
+        {
+            yield return Value;
+
+            if (Left != null)
+            {
+                foreach (T value in Left.PreOrderTraversal())
+                {
+                    yield return value;
+                }
+            }
+
+            if (Right != null)
+            {
+                foreach (T value in Right.PreOrderTraversal())
+                {
+                    yield return value;
+                }
+            }
+        }
+
+        public IEnumerable<T> PostOrderTraversal()
+        {
+            if (Left != null)
+            {
+                foreach (T value in Left.PostOrderTraversal())
+                {
+                    yield return value;
+                }
+            }
+
+            if (Right != null)
+            {
+                foreach (T value in Right.PostOrderTraversal())
+                {
+                    yield return value;
+                }
+            }
+
+            yield return Value;
+        }
+    }
+
+    public class BinaryTree<T> where T : IComparable<T>
+    {
+        private int count = 0;
+
+        public int Count { 
+            get => count;
+        }
+
+        public Node<T> Root { get; private set; } = null!;
+
+        public void Add(T value)
+        {
+            if (Root == null)
+            {
+                Root = new Node<T>(value);
+            }
+            else
+            {
+                Root.Add(value);
+            }
+
+            count++;
+        }
+
+        public IEnumerable<T> InOrderTraversal()
+        {
+            if (Root != null)
+            {
+                foreach (T value in Root.InOrderTraversal())
+                {
+                    yield return value;
+                }
+            }
+        }
+
+        public IEnumerable<T> PreOrderTraversal()
+        {
+            if (Root != null)
+            {
+                foreach (T value in Root.PreOrderTraversal())
+                {
+                    yield return value;
+                }
+            }
+        }
+
+        public IEnumerable<T> PostOrderTraversal()
+        {
+            if (Root != null)
+            {
+                foreach (T value in Root.PostOrderTraversal())
+                {
+                    yield return value;
+                }
+            }
+        }
+
+        public void CountTree()
+        {
+            if (Root != null)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Tree is empty.");
+            }
+        }
+    }
+}
---
+++ CollectionApp/CircularLinkedList.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionApp
+{
+    public static class CircularLinkedListExtensions
+    {
+        public static LinkedListNode<T> Next<T>(this LinkedListNode<T> node)
+        {
+            if (node != null && node.List != null)
+            {
+                return node.Next ?? node.List.First;
+            }
+
+            return null;
+        }
+
+        public static LinkedListNode<T> Previous<T>(this LinkedListNode<T> node)
+        {
+            if (node != null && node.List != null)
+            {
+                return node.Previous ?? node.List.Last;
+            }
+            return null;
+        }
+    }
+
+    public class CircularLinkedList<T> : LinkedList<T>
+    {
+        public T this[int index]
+        {
+            get
+            {
+                LinkedListNode<T> current = First;
+                for (int i = 0; i < index; i++)
+                {
+                    current = current.Next ?? First;
+                }
+                return current.Value;
+            }
+        }
+
+        public new IEnumerator GetEnumerator()
+        {
+            return new CircularLinkedListEnumerator<T>(this);
+        }
+
+        public void Add(T value)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(value);
+
+            if (Count == 0)
+            {
+                AddFirst(newNode);
+            }
+            else
+            {
+                AddAfter(Last, newNode);
+            }
+        }
+
+        public void Remove(T value)
+        {
+            LinkedListNode<T> nodeToRemove = Find(value);
+
+            if (nodeToRemove != null)
+            {
+                Remove(nodeToRemove);
+            }
+        }
+
+        public void Print()
+        {
+            if (Count == 0)
+            {
+                Console.WriteLine("List is empty.");
+                return;
+            }
+
+            LinkedListNode<T> current = First;
+
+            do
+            {
+                Console.Write(current.Value + " ");
+                current = current.Next ?? First;
+            }
+            while (current != First);
+        }
+    }
+
+    public class CircularLinkedListEnumerator<T> : IEnumerator<T>
+    {
+        private LinkedListNode<T> _current;
+        public T Current => _current.Value;
+        object IEnumerator.Current => Current;
+
+        public CircularLinkedListEnumerator(LinkedList<T> list)
+        {
+            _current = list.First;
+        }
+
+        public bool MoveNext()
+        {
+            if (_current == null)
+            {
+                return false;
+            }
+
+            _current = _current.Next ?? _current.List.First;
+            return true;
+        }
+
+        public void Reset()
+        {
+            _current = _current.List.First;
+        }
+
+        public void Dispose() { }
+    }
+}
