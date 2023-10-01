904b1a939ca21a0b1cd7e0dbb06c0ba3be76ff7a
Андрей Тарасенко <mikatohimemiya@gmail.com>
2023-10-01 14:22:19 +0300

    Added solution for algorithms HW

---
+++ AlgorithmsApp/Algorithms/Recursion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsApp.Algorithms
+{
+    public static class Recursion
+    {
+        public static int Fibonacci(int value)
+        {
+            if (value == 0 || value == 1) 
+                return value;
+
+            return Fibonacci(value - 1) + Fibonacci(value - 2);
+        }
+
+
+        public static int Factorial(int value)
+        {
+            if (value == 1) 
+                return 1;
+
+            return value * Factorial(value - 1);
+        }
+    }
+}
---
+++ AlgorithmsApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using AlgorithmsApp.Algorithms;
+
+namespace AlgorithmsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Recursion.Fibonacci(20));
+            Console.WriteLine(Recursion.Factorial(20));
+
+            var numbers = new int[] { 5, 4, 5, 7, 6, 9, 4, 1, 1, 3, 4, 50, 41 };
+            QuickSortClass.QuickSort(numbers);
+            Console.WriteLine(string.Join($", ", numbers));
+            Console.WriteLine(BinarySearchClass.BinarySearch(numbers,0));
+        }
+    }
+}
